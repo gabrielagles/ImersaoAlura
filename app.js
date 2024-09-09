e719580bf453bbbb5ff38260248c5e6ae4832da1
Gabriel Agles <gabriel.aglesss@gmail.com>
2024-09-09 15:39:29 -0300

    Projeto Alura/Gemini

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-console.log(dados);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById
+    ("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um livro.</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+            titulo = dado.titulo.toLowerCase()
+            descricao = dado.descricao.toLowerCase()
+            tags = dado.tags.toLowerCase()
+            // se titulo includes campoPesquisa
+            if (titulo.includes(campoPesquisa) || descricao.
+            includes(campoPesquisa) || tags.includes(campoPesquisa) ) {
+                resultados += `
+                <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p>${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+            // Cria um novo elemento HTML para cada resultado
+        
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+
+}
+
