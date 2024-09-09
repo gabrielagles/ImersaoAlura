03115825cb5523b24e5f34d74ad97fcaed5f743d
Gabriel Agles <gabriel.aglesss@gmail.com>
2024-09-09 11:14:37 -0300

    teste

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+
+    titulo : "O ladrão de Raios",
+    descricao : "Percy Jackson, um garoto com dificuldades de aprendizado, descobre ser um semideus, filho dePoseidon.Injustamente acusado de roubar o raio de Zeus, ele embarca em uma jornada épica ao lado de Annabethe Grover para limparseu nome e evitar uma guerra entre os deuses. Enfrentando monstros e desvendando mistérios, Percy precisa encontrar o raio perdido antes que seja tarde demais",
+    link : "https://acampamentomeiosangue.fandom.com/pt/wiki/O_Ladr%C3%A3o_de_Raios"
+
+    },
+    {
+
+    titulo: "O Mar de Monstros",
+    descricao: "O acampamento meio-sangue é atacado por monstros marinhos. Percy Jackson, Annabeth Chase e Grover Underwood embarcam em uma perigosa missão para encontrar o Velocino de Ouro e salvar seu lar.",
+    link: "https://acampamentomeiosangue.fandom.com/pt/wiki/O_Mar_de_Monstros"
+
+    },
+    {
+
+    titulo: "A Maldição do Titã",
+    descricao: "Um antigo titã desperta e ameaça destruir o mundo. Percy e seus amigos devem encontrar o Titã Atlas e aprisioná-lo novamente antes que seja tarde demais.",
+    link: "https://acampamentomeiosangue.fandom.com/pt/wiki/A_Maldi%C3%A7%A3o_do_Tit%C3%A3"
+
+    },
+    {
+
+    titulo: "A Batalha do Labirinto",
+    descricao: "Um labirinto mortal se espalha pelo acampamento meio-sangue. Percy e seus amigos devem encontrar o centro do labirinto e impedir que Cronos escape.",
+    link: "https://acampamentomeiosangue.fandom.com/pt/wiki/A_Batalha_do_Labirinto"
+
+    },
+    {
+
+    titulo: "O Último Olimpiano",
+    descricao: "A guerra entre deuses e titãs está prestes a começar. Percy Jackson deve liderar os semideuses em uma batalha épica para salvar o mundo.",
+    link: "https://acampamentomeiosangue.fandom.com/pt/wiki/O_Último_Olimpiano"
+    
+    }
+]
